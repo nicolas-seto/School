d8854e89b7a4c8a6a3e4747225abf9ea0d23c928
nicolas_seto <nicolasseto@gmail.com>
2014-03-21 23:06:54 -0700

    Finished LFU.

--- CS149/Java/SwapPage/src/LFU.java
+++ CS149/Java/SwapPage/src/LFU.java
@@ -1,4 +1,133 @@
 
 public class LFU {
 
+    private PageProcess process;
+    private static final int FOUR = 4;
+    private static final int TOTAL_REF = 100;
+    private static StringBuffer buffer;
+    private int[] pageFrame;
+    private int[] counter;
+    private int hits;
+    private int hitPage;
+    private int pagedIn;
+    private int pagedOut;
+    private boolean isFilled;
+    
+    public LFU(PageProcess aProcess) {
+        process = aProcess;
+        pageFrame = new int[FOUR];
+        counter = new int[FOUR];
+        this.initializePageFrame();
+        hits = 0;
+        hitPage = pagedIn = pagedOut = -1;
+        isFilled = false;
+    }
+    
+    // Set values to -1 so that we know it's empty.
+    private void initializePageFrame() {
+        for (int i = 0; i < FOUR; i++) {
+            pageFrame[i] = -1;
+        }
+    }
+    
+    public void run() {
+        this.runProcess();
+    }
+    
+    private void runProcess() {
+        
+        for (int i = 0; i < TOTAL_REF; i++) {
+            int page = process.run();
+            int index = containsPage(page);
+            if (index != -1) {
+                hits++;
+                pagedIn = pagedOut = -1;
+                hitPage = page;
+                
+                counter[index]++;
+            } else {
+                this.insertToPageFrame(page);
+            }
+            
+            this.displayPageFrameContents();
+        }
+    }
+    
+    private int containsPage(int j) {
+        for (int i = 0; i < FOUR; i++) {
+            if (pageFrame[i] == j) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+    private void insertToPageFrame(int page) {
+        if (!isPageFrameFilled()) {
+            for (int i = 0; i < FOUR; i++) {
+                if (pageFrame[i] == -1) {
+                    pageFrame[i] = page;
+                    counter[i] = 1; // Initialize page at that index to count 1;
+                    
+                    if (i == FOUR - 1) {
+                        isFilled = true;
+                    }
+                    
+                    break;
+                }
+            }
+        } else {
+            int oldIndex = this.leastFrequentlyUsed(); // The element with the least hits
+            pagedOut = pageFrame[oldIndex];
+            
+            pageFrame[oldIndex] = page;
+            counter[oldIndex] = 1;
+        }
+        
+        pagedIn = page;
+        hitPage = -1;
+    }
+    
+    private boolean isPageFrameFilled() {
+        return isFilled;
+    }
+    
+    private int leastFrequentlyUsed() {
+        int max = TOTAL_REF;
+        int index = 0;
+        for (int i = 0; i < FOUR; i++) {
+            if (counter[i] < max) {
+                max = counter[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+    
+    private void displayPageFrameContents() {
+        buffer = new StringBuffer();
+        buffer.append('[');
+        for (int i = 0; i < FOUR; i++) {
+            if (i != FOUR - 1) {
+                buffer.append(pageFrame[i] + ",");
+            } else {
+                buffer.append(pageFrame[i] + "] ");
+            }
+        }
+        if (pagedIn != -1) {
+            buffer.append("Paged in: " + pagedIn + " ");
+        }
+        if (pagedOut != -1) {
+            buffer.append("Page evicted: " + pagedOut + " ");
+        }
+        if (hitPage != -1) {
+            buffer.append("Hit page: " + hitPage + " ");
+        }
+        
+        System.out.printf("%s\n", buffer.toString());
+    }
+    
+    public double hitRatio() {
+        return ((double) hits / (double) TOTAL_REF) * 100;
+    }
 }
--- CS149/Java/SwapPage/src/Paging.java
+++ CS149/Java/SwapPage/src/Paging.java
@@ -3,6 +3,7 @@ public class Paging {
     
     private static FIFO a;
     private static LRU b;
+    private static LFU c;
     private static PageProcess aProcess;
     
     public static void main(String[] args) {
@@ -13,13 +14,20 @@ public class Paging {
             System.out.printf("Run %d of FIFO:\n", i);
             a.run();
             System.out.printf("Hit ratio: %.2f%%\n\n", a.hitRatio());
-        }*/
+        }
         for (int i = 1; i <= 5; i++) {
             aProcess = new PageProcess('A');
             b = new LRU(aProcess);
             System.out.printf("Run %d of LRU:\n", i);
             b.run();
             System.out.printf("Hit ratio: %.2f%%\n\n", b.hitRatio());
+        }*/
+        for (int i = 1; i <= 5; i++) {
+            aProcess = new PageProcess('A');
+            c = new LFU(aProcess);
+            System.out.printf("Run %d of LFU:\n", i);
+            c.run();
+            System.out.printf("Hit ratio: %.2f%%\n\n", c.hitRatio());
         }
     }
 }
