04089ec3de87856fba14b19240aa2d9f64e85d20
nicolas_seto <nicolasseto@gmail.com>
2014-03-21 23:43:06 -0700

    Finished Paging

--- CS149/Java/SwapPage/src/Paging.java
+++ CS149/Java/SwapPage/src/Paging.java
@@ -5,37 +5,80 @@ public class Paging {
     private static LRU b;
     private static LFU c;
     private static MFU d;
+    private static RandomPick e;
     private static PageProcess aProcess;
+    private static double total;
     
-    public static void main(String[] args) {
-        
-        /*for (int i = 1; i <= 5; i++) {
+    private static void runFIFO() {
+        total = 0;
+        for (int i = 1; i <= 5; i++) {
             aProcess = new PageProcess('A');
             a = new FIFO(aProcess);
             System.out.printf("Run %d of FIFO:\n", i);
             a.run();
             System.out.printf("Hit ratio: %.2f%%\n\n", a.hitRatio());
+            total += a.hitRatio();
         }
+        System.out.printf("Average hit ratio over 5 runs: %.2f%%\n\n", (total / 500.0) * 100);
+    }
+    
+    private static void runLRU() {
+        total = 0;
         for (int i = 1; i <= 5; i++) {
             aProcess = new PageProcess('A');
             b = new LRU(aProcess);
             System.out.printf("Run %d of LRU:\n", i);
             b.run();
             System.out.printf("Hit ratio: %.2f%%\n\n", b.hitRatio());
+            total += b.hitRatio();
         }
+        System.out.printf("Average hit ratio over 5 runs: %.2f%%\n\n", (total / 500.0) * 100);
+    }
+    
+    private static void runLFU() {
+        total = 0;
         for (int i = 1; i <= 5; i++) {
             aProcess = new PageProcess('A');
             c = new LFU(aProcess);
             System.out.printf("Run %d of LFU:\n", i);
             c.run();
             System.out.printf("Hit ratio: %.2f%%\n\n", c.hitRatio());
-        }*/
+            total += c.hitRatio();
+        }
+        System.out.printf("Average hit ratio over 5 runs: %.2f%%\n\n", (total / 500.0) * 100);
+    }
+    
+    private static void runMFU() {
+        total = 0;
         for (int i = 1; i <= 5; i++) {
             aProcess = new PageProcess('A');
             d = new MFU(aProcess);
             System.out.printf("Run %d of MFU:\n", i);
             d.run();
             System.out.printf("Hit ratio: %.2f%%\n\n", d.hitRatio());
+            total += d.hitRatio();
         }
+        System.out.printf("Average hit ratio over 5 runs: %.2f%%\n\n", (total / 500.0) * 100);
+    }
+    
+    private static void runRandomPick() {
+        total = 0;
+        for (int i = 1; i <= 5; i++) {
+            aProcess = new PageProcess('A');
+            e = new RandomPick(aProcess);
+            System.out.printf("Run %d of Random Pick:\n", i);
+            e.run();
+            System.out.printf("Hit ratio: %.2f%%\n\n", e.hitRatio());
+            total += e.hitRatio();
+        }
+        System.out.printf("Average hit ratio over 5 runs: %.2f%%\n\n", (total / 500.0) * 100);
+    }
+    
+    public static void main(String[] args) {  
+        runFIFO();
+        runLRU();
+        runLFU();
+        runMFU();
+        runRandomPick();
     }
 }
--- CS149/Java/SwapPage/src/RandomPick.java
+++ CS149/Java/SwapPage/src/RandomPick.java
@@ -1,4 +1,118 @@
+import java.util.Random;
 
 public class RandomPick {
 
+    private PageProcess process;
+    private static final int FOUR = 4;
+    private static final int TOTAL_REF = 100;
+    private static StringBuffer buffer;
+    private int[] pageFrame;
+    private int hits;
+    private int hitPage;
+    private int pagedIn;
+    private int pagedOut;
+    private boolean isFilled;
+    private Random rand;
+    
+    public RandomPick(PageProcess aProcess) {
+        process = aProcess;
+        pageFrame = new int[FOUR];
+        this.initializePageFrame();
+        hits = 0;
+        hitPage = pagedIn = pagedOut = -1;
+        isFilled = false;
+        rand = new Random();
+    }
+    
+    // Set values to -1 so that we know it's empty.
+    private void initializePageFrame() {
+        for (int i = 0; i < FOUR; i++) {
+            pageFrame[i] = -1;
+        }
+    }
+    
+    public void run() {
+        this.runProcess();
+    }
+    
+    private void runProcess() {
+        
+        for (int i = 0; i < TOTAL_REF; i++) {
+            int page = process.run();
+            int index = containsPage(page);
+            if (index != -1) {
+                hits++;
+                pagedIn = pagedOut = -1;
+                hitPage = page;
+            } else {
+                this.insertToPageFrame(page);
+            }
+            
+            this.displayPageFrameContents();
+        }
+    }
+    
+    private int containsPage(int j) {
+        for (int i = 0; i < FOUR; i++) {
+            if (pageFrame[i] == j) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+    private void insertToPageFrame(int page) {
+        if (!isPageFrameFilled()) {
+            for (int i = 0; i < FOUR; i++) {
+                if (pageFrame[i] == -1) {
+                    pageFrame[i] = page;
+                    
+                    if (i == FOUR - 1) {
+                        isFilled = true;
+                    }
+                    
+                    break;
+                }
+            }
+        } else {
+            int oldIndex = rand.nextInt(FOUR); // Pick a random page
+            pagedOut = pageFrame[oldIndex];
+            
+            pageFrame[oldIndex] = page;
+        }
+        
+        pagedIn = page;
+        hitPage = -1;
+    }
+    
+    private boolean isPageFrameFilled() {
+        return isFilled;
+    }
+    
+    private void displayPageFrameContents() {
+        buffer = new StringBuffer();
+        buffer.append('[');
+        for (int i = 0; i < FOUR; i++) {
+            if (i != FOUR - 1) {
+                buffer.append(pageFrame[i] + ",");
+            } else {
+                buffer.append(pageFrame[i] + "] ");
+            }
+        }
+        if (pagedIn != -1) {
+            buffer.append("Paged in: " + pagedIn + " ");
+        }
+        if (pagedOut != -1) {
+            buffer.append("Page evicted: " + pagedOut + " ");
+        }
+        if (hitPage != -1) {
+            buffer.append("Hit page: " + hitPage + " ");
+        }
+        
+        System.out.printf("%s\n", buffer.toString());
+    }
+    
+    public double hitRatio() {
+        return ((double) hits / (double) TOTAL_REF) * 100;
+    }
 }
