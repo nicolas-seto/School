de46f8b241db8d6415d9856e85dd209c8aacd8f8
nicolas_seto <nicolasseto@gmail.com>
2014-03-21 23:11:47 -0700

    Finished MFU

--- CS149/Java/SwapPage/src/Paging.java
+++ CS149/Java/SwapPage/src/Paging.java
@@ -4,6 +4,7 @@ public class Paging {
     private static FIFO a;
     private static LRU b;
     private static LFU c;
+    private static MFU d;
     private static PageProcess aProcess;
     
     public static void main(String[] args) {
@@ -21,13 +22,20 @@ public class Paging {
             System.out.printf("Run %d of LRU:\n", i);
             b.run();
             System.out.printf("Hit ratio: %.2f%%\n\n", b.hitRatio());
-        }*/
+        }
         for (int i = 1; i <= 5; i++) {
             aProcess = new PageProcess('A');
             c = new LFU(aProcess);
             System.out.printf("Run %d of LFU:\n", i);
             c.run();
             System.out.printf("Hit ratio: %.2f%%\n\n", c.hitRatio());
+        }*/
+        for (int i = 1; i <= 5; i++) {
+            aProcess = new PageProcess('A');
+            d = new MFU(aProcess);
+            System.out.printf("Run %d of MFU:\n", i);
+            d.run();
+            System.out.printf("Hit ratio: %.2f%%\n\n", d.hitRatio());
         }
     }
 }
